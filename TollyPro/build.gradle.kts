21e824614e6bb1e57b611cc7fc51412b0964459b
doGior <43815981+doGior@users.noreply.github.com>
2025-05-19 16:37:12 +0200

    Disabled

--- TollyPro/build.gradle.kts
+++ TollyPro/build.gradle.kts
@@ -12,7 +12,7 @@ cloudstream {
      * 2: Slow
      * 3: Beta only
     **/
-    status  = 1 // will be 3 if unspecified
+    status  = 0 // will be 3 if unspecified
     tvTypes = listOf("NSFW")
     iconUrl = "https://t3.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://tellyhd.pro&size=16"
 }
