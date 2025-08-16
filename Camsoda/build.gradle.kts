284ac6810637ce5274448ca3005bfa06198dddfc
Riccardo Giordano <43815981+doGior@users.noreply.github.com>
2025-08-16 18:27:28 +0200

    Disabled

--- Camsoda/build.gradle.kts
+++ Camsoda/build.gradle.kts
@@ -12,7 +12,7 @@ cloudstream {
      * 2: Slow
      * 3: Beta only
     **/
-    status  = 1 // will be 3 if unspecified
+    status  = 0 // will be 3 if unspecified
     tvTypes = listOf("NSFW")
     iconUrl = "https://cachew.livemediahost.com/assets/img/camsoda-logo-160x50.png"
 }
